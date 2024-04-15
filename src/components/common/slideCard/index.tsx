1357e7d204b536e00a6627497a5f415cd3298917
ovitormacel <ovitormacel00@gmail.com>
2024-04-15 10:25:07 -0300

    Inicio e criação dos Componentes Slide. Conexão com o Backend

---
+++ src/components/common/slideCard/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+import { CourseType } from "@/services/courseService";
+
+interface props {
+    course: CourseType;
+}
+
+const SlideCard = ({ course }: props) => {
+    return (
+        <>
+            <div className={styles.slide}>
+                <img className={styles.slideImg} src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} />
+                <p className={styles.slideTitle}>{course.name}</p>
+                <p className={styles.slideDescription}>{course.synopsis}</p> 
+            </div>
+        </>
+    )
+}
+
+export default SlideCard;
